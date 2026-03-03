e0513f09f4eb843aa542f146ad855ff991e39c86
Sheddo7 <shadrachdogara77@gmail.com>
2026-03-03 11:54:12 -0800

    Fix storage using Django 5.2 STORAGES dict

--- blog_core/settings.py
+++ blog_core/settings.py
@@ -18,6 +18,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
     'blog',
     'accounts',
     'taggit',
@@ -77,25 +78,32 @@ else:
         }
     }
 
-# --- Static Files (WhiteNoise) ---
+# --- Static and Media Files ---
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# --- Media Files ---
-# --- Media Files (Supabase Storage) ---
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_ACCESS_KEY_ID = os.environ.get('SUPABASE_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('SUPABASE_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('SUPABASE_BUCKET')
-AWS_S3_ENDPOINT_URL = 'https://oqqrptqnairmerdfbdgi.supabase.co/storage/v1/s3'
-AWS_S3_REGION_NAME = 'eu-west-1'
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = 'public-read'
-AWS_QUERYSTRING_AUTH = False
-AWS_S3_ADDRESSING_STYLE = 'path'  # ← Important for Supabase
-MEDIA_URL = 'https://oqqrptqnairmerdfbdgi.supabase.co/storage/v1/object/public/media/'
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+        "OPTIONS": {
+            "access_key": os.environ.get('SUPABASE_ACCESS_KEY_ID'),
+            "secret_key": os.environ.get('SUPABASE_SECRET_ACCESS_KEY'),
+            "bucket_name": os.environ.get('SUPABASE_BUCKET'),
+            "endpoint_url": "https://oqqrptqnairmerdfbdgi.supabase.co/storage/v1/s3",
+            "region_name": "eu-west-1",
+            "file_overwrite": False,
+            "default_acl": "public-read",
+            "querystring_auth": False,
+            "addressing_style": "path",
+        },
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
+MEDIA_URL = "https://oqqrptqnairmerdfbdgi.supabase.co/storage/v1/object/public/media/"
 
 # --- Crispy Forms ---
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
