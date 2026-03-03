22a0f954c96b1d280aa85fa2d7850c8254fa25a1
Sheddo7 <shadrachdogara77@gmail.com>
2026-03-03 03:58:32 -0800

    Fix storage settings for Django 5.2

--- blog_core/settings.py
+++ blog_core/settings.py
@@ -83,7 +83,7 @@ else:
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # --- Cloudinary Media Storage ---
 CLOUDINARY_STORAGE = {
@@ -91,7 +91,14 @@ CLOUDINARY_STORAGE = {
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 MEDIA_URL = '/media/'
 
 # --- Crispy Forms ---
